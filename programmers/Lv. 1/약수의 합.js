70e5f468e43c88e26b7dbc8c02b128f979458c89
jungwonyu <sunsetinmygarden@gmail.com>
2023-03-14 17:00:54 +0900

    Lv. 1

--- programmers/Lv. 1/약수의 합.js
+++ programmers/Lv. 1/약수의 합.js
@@ -7,8 +7,8 @@
 function solution(n) {
     let answer = 0;
     for (let i = 0; i <= n; i++) {
-        if (n % i === 0) {
-            answer = answer + i;
+        if (n % i == 0) {
+            answer += i;
         }
     }
     return answer;
